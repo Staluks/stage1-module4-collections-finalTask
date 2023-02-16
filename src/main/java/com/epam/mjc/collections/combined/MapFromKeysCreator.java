17a1bd35159906e5aaaae7bf94250ab28c9167f4
Staluks <janchukssss@inbox.lv>
2023-02-16 16:24:06 +0200

    Finished final task 1.4

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer,Set<String>> karte = new HashMap<>();
+        for(Map.Entry<String, Integer> me : sourceMap.entrySet()){
+           int k = me.getKey().length();
+            Set<String> list = new HashSet<>();
+            for(Map.Entry<String, Integer> keyList : sourceMap.entrySet()){
+                if(keyList.getKey().length() == k){
+                    list.add(keyList.getKey());
+                }
+            }
+            karte.put(k, list);
+        }
+        return karte;
     }
 }
